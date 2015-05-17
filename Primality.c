9ee03bec092e65c0498658bad69844c04d7a0b57
Rohmany <mohamadabdalalim@gmail.com>
2015-05-18 00:37:18 +0200

    Update Primality.c

--- Primality.c
+++ Primality.c
@@ -7,11 +7,11 @@ int isPrime(long long int n) {
     else if (n <= 3) {
         return 1;
     }
-    else if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0) {
+    else if (n % 2 == 0 || n % 3 == 0) {
         return 0;
     }
     long long int a = sqrt(n); // That's why
-    for (long long int i = 11; i <= a; i+=6) {
+    for (long long int i = 5; i <= a; i+=6) {
         if (n % i == 0 || n % (i+2) == 0) {
             return 0;
         }
